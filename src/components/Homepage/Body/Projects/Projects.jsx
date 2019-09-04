d0fc7ff0c6241bb2987098a9dd56bb138c004457
DeolaJ <adejoe97@yahoo.com>
2019-09-04 18:59:33 +0100

    Fixed the project section
    Signed-off-by: DeolaJ <adejoe97@yahoo.com>

--- src/components/Homepage/Body/Projects/Projects.jsx
+++ src/components/Homepage/Body/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Grid, Header, Container, Card } from 'semantic-ui-react'
+import { Grid, Header, Container, Card, Button } from 'semantic-ui-react'
 import './Projects.scss'
 
 class Projects extends Component {
@@ -11,38 +11,53 @@ class Projects extends Component {
       <Grid stackable className={'section projects-container'} style={{minHeight: '100vh'}}>
         <Grid.Column width={16}>
           <Container>
-            <Header as='h2' textAlign='center'>Ongoing Projects</Header>
+            <Header as='h2' className={'section-title'} textAlign='center'>Ongoing Projects</Header>
             <Grid stackable style={{ marginTop: '50px'}}>
               <Grid.Row columns={3}>
                 <Grid.Column>
                   <Card raised centered>
-                    <Card.Header textAlign='center'>ICON</Card.Header>
+                    <div className={'placeholder-image'}></div>
+                    <p className={'date-label'}>April 2019</p>
                     <Card.Content>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                      Aut accusantium distinctio excepturi nemo nam molestias enim, 
+                      <Card.Header as="h4">ICON</Card.Header>
+                      <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Aut accusantium distinctio excepturi nemo nam molestias enim, 
+                      </p>
                     </Card.Content>
                   </Card>
                 </Grid.Column>
                 <Grid.Column>
                   <Card raised centered>
-                    <Card.Header textAlign='center'>ICON</Card.Header>
+                    <div className={'placeholder-image'}></div>
+                    <p className={'date-label'}>April 2019</p>
                     <Card.Content>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                      Aut accusantium distinctio excepturi nemo nam molestias enim, 
+                      <Card.Header as="h4">ICON</Card.Header>
+                      <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Aut accusantium distinctio excepturi nemo nam molestias enim, 
+                      </p>
                     </Card.Content>
                   </Card>
                 </Grid.Column>
                 <Grid.Column>
                   <Card raised centered>
-                    <Card.Header textAlign='center'>ICON</Card.Header>
+                    <div className={'placeholder-image'}></div>
+                    <p className={'date-label'}>April 2019</p>
                     <Card.Content>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                      Aut accusantium distinctio excepturi nemo nam molestias enim, 
+                      <Card.Header as="h4">ICON</Card.Header>
+                      <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Aut accusantium distinctio excepturi nemo nam molestias enim, 
+                      </p>
                     </Card.Content>
                   </Card>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
+          </Container>
+          <Container className={'bottom-button'} textAlign="center">
+            <Button size="huge">See more</Button>
           </Container>
         </Grid.Column>
       </Grid>
